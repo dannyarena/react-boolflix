f0f6def811075671c1833ac8162962f115a5cb00
dannyarena <arenadanny71@gmail.com>
2025-06-05 23:01:44 +0200

    refactor SearchBar component now able to find both movies and tv series

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -8,13 +8,26 @@ export default function SearchBar() {
             const API_KEY = `e5dd1dbd92245a50236e0af84095f35a`;
             const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=it-IT`;
             const tvURL = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${query}&language=it-IT`;
-            axios.get(url)
-            .then(response => {
-                setMovies(response.data.results);
-            })
-            .catch(error => {
-                console.error("Errore nella ricezione dei dati:", error);
-            });
+            
+            Promise.all([
+                axios.get(movieURL),
+                axios.get(tvURL)
+            ])
+                .then(([movieRes, tvRes]) => {
+                    const movieResults = movieRes.data.results;
+                    const tvResults = tvRes.data.results;
+
+                    const allResult = [
+                        ...movieResults.map(item => ({ ...item, media_type: 'movie'})),
+                        ...tvResults.map(item => ({ ...item, media_type: 'tv'}))
+                        
+                    ];
+                        // salva tutti i risultati uniti in movies
+                    setMovies(allResult);
+                })
+                .catch(error => {
+                    console.error("Errore nella ricezione dei dati")
+                });
         }
         const [movies, setMovies] = useState([]);
     return (
@@ -31,8 +44,8 @@ export default function SearchBar() {
             <ul>
                 {movies.map(movie => (
                     <li key={movie.id}>
-                        <p><strong>Titolo:</strong>{movie.title}</p>
-                        <p><strong>Titolo originale:</strong>{movie.original_title}</p>
+                        <p><strong>Titolo:</strong>{movie.title || movie.name}</p>
+                        <p><strong>Titolo originale:</strong>{movie.original_title || movie.original_name}</p>
                         <p><strong>Lingua:</strong><img src={`https://flagcdn.com/16x12/${movie.original_language}.png`} alt={movie.original_language}  style={{marginLeft: '5px'}}
                         />
                         </p>
