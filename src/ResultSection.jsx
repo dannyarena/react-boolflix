db4deeb3351403ceaf3d2cddffca9b2f5f3d46b6
dannyarena <arenadanny71@gmail.com>
2025-06-06 22:11:30 +0200

    add and import result section component

---
+++ src/ResultSection.jsx
@@ -0,0 +1,23 @@
+import { useMedia } from "./context/MediaContext";
+
+export default function ResultSection() {
+    const { movies } = useMedia();
+    
+     return (
+        <div>
+            {/* risultati in pagina */}
+            <ul>
+                {movies.map(movie => (
+                    <li key={movie.id}>
+                        <p><strong>Titolo:</strong>{movie.title || movie.name}</p>
+                        <p><strong>Titolo originale:</strong>{movie.original_title || movie.original_name}</p>
+                        <p><strong>Lingua:</strong>
+                            <img src={`https://flagcdn.com/16x12/${movie.original_language}.png`} alt={movie.original_language}  style={{marginLeft: '5px'}} />
+                        </p>
+                        <p><strong>Voto:</strong>{movie.vote_average}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -14,20 +14,6 @@ export default function SearchBar() {
             onChange={(event) => setQuery(event.target.value)}/>
             {/* bottone cerca */}
             <button onClick={() => searchMedia(query)}>Cerca</button>
-
-            {/* risultati in pagina */}
-            <ul>
-                {movies.map(movie => (
-                    <li key={movie.id}>
-                        <p><strong>Titolo:</strong>{movie.title || movie.name}</p>
-                        <p><strong>Titolo originale:</strong>{movie.original_title || movie.original_name}</p>
-                        <p><strong>Lingua:</strong><img src={`https://flagcdn.com/16x12/${movie.original_language}.png`} alt={movie.original_language}  style={{marginLeft: '5px'}}
-                        />
-                        </p>
-                        <p><strong>Voto:</strong>{movie.vote_average}</p>
-                    </li>
-                ))}
-            </ul>
         </div>
     );
 }
