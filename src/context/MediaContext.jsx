49c4cfc707d622f4f0e219f4da65b3c97bb253b8
dannyarena <arenadanny71@gmail.com>
2025-06-06 17:14:28 +0200

    add MediaProvider to context for managing movies and query state

--- src/context/MediaContext.jsx
+++ src/context/MediaContext.jsx
@@ -5,3 +5,14 @@ const MediaContext = createContext();
 export function useMedia() {
     return useContext (MediaContext);
 }
+
+export function MediaProvider({ children }) {
+    const [movies, setMovies] = useState([]);
+    const [query, setQuery] = useState('');
+
+    return (
+        <MediaContext.Provider value={{ movies, setMovies, query, setQuery}}>
+            {children}
+        </MediaContext.Provider>
+    );
+}
